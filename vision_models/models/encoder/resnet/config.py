c8e47befb0b8ee54aa19b57a3182dc25a63277c0
nagohachi <nagohachi.study@gmail.com>
2026-01-17 19:01:21 +0900

    added config

---
+++ vision_models/models/encoder/resnet/config.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import Literal
+
+
+@dataclass
+class ResNetBlockConfig:
+    out_channels: int
+    kernel_sizes: list[tuple[int, int]]
+    stride: int
+
+
+@dataclass
+class ResNetStageConfig:
+    block_configs: list[ResNetBlockConfig]
+
+
+@dataclass
+class ResNetConfig:
+    version: Literal["v1", "v2"]
+    stages: list[ResNetStageConfig]
